761206f975b20e5c7fba0a4302e1b833170a7850
trungnguyen <quytrung2406@gmail.com>
2025-11-18 02:45:28 +0700

    Complete the previous commit

--- AimsProject/src/com/hust/kstn/models/CompactDisc.java
+++ AimsProject/src/com/hust/kstn/models/CompactDisc.java
@@ -6,9 +6,9 @@ public class CompactDisc {
     private String title;
     private String category;
     private double cost;
-    private String[] directors;
-    private String[] artists;
-    private Track[] tracks;
+    private String[] directors = new String[10];
+    private String[] artists = new String[10];
+    private Track[] tracks = new Track[20];
     public CompactDisc(String title, String category, double cost, String[] directors, String[] artists, Track[] tracks) {
         this.title = title;
         this.category = category;
@@ -17,6 +17,41 @@ public class CompactDisc {
         this.artists = artists;
         this.tracks = tracks;
         this.id = ++nbCompactDiscs;
+    }
+    public int totalLength() {
+        int total = 0;
+        if (tracks == null) {
+            System.out.println("This CD has no tracks");
+            return total;
+        }
+        for (Track track : tracks) {
+            total += track.getLength();
+        }
+        return total;
+    }
+    public boolean addTrack(Track track) {
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] == null) {
+                tracks[i] = track;
+                return true;
+            }
+        }
+        System.out.println("The track list is full");
+        return false;
+    }
+    public boolean removeTrack(Track track) {
+        if (tracks == null) {
+            System.out.println("This CD has no tracks");
+            return false;
+        }
+        for (int i = 0; i < tracks.length; i++) {
+            if (tracks[i] == track) {
+                tracks[i] = null;
+                return true;
+            }
+        }
+        System.out.println("Track not found");
+        return false;
     }
     public String getTitle() {
         return title;
@@ -36,7 +71,7 @@ public class CompactDisc {
     public Track[] getTracks() {
         return tracks;
     }
-    
+
     @Override
     public String toString() {
         return "CD[" + this.id + "][" 
--- AimsProject/src/com/hust/kstn/test/CompactDiscTest.java
+++ AimsProject/src/com/hust/kstn/test/CompactDiscTest.java
@@ -7,8 +7,26 @@ public class CompactDiscTest {
     public static void main(String[] args) {
         Track track1 = new Track("My Old Man", 210);
         Track track2 = new Track("Carey", 180);
+        Track track3 = new Track("Blue", 240);
+        Track track4 = new Track("California", 200);
+        Track track5 = new Track("A Case of You", 230);
+        Track track6 = new Track("River", 190);
+        Track track7 = new Track("The Last Time I Saw Richard", 220);
         System.out.println(track1);
+        System.out.println(track2);
+        System.out.println(track3);
+        System.out.println(track4);
+        System.out.println(track5);
+        System.out.println(track6);
+        System.out.println(track7);
         CompactDisc cd = new CompactDisc("Blue", "Folk", 15.99,new String[]{"EMI"}, new String[]{"Joni Mitchell"}, new Track[]{track1, track2});
+        cd.addTrack(track3);
+        cd.addTrack(track4);
+        cd.addTrack(track5);
+        cd.addTrack(track6);
         System.out.println(cd);
+        cd.removeTrack(track4);
+        cd.removeTrack(track7);
+        System.out.println("Total length: " + cd.totalLength() + " s");
     }
 }
