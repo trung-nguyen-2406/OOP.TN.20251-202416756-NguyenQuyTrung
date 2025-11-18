e1ae9df9e4f6a3d55e9ad3ca4dc848ee1866b81b
trungnguyen <quytrung2406@gmail.com>
2025-11-18 14:58:38 +0700

    Pass by value sandbox

---
+++ OtherProject/src/com/hust/kstn/parameters/Student.java
@@ -0,0 +1,16 @@
+package com.hust.kstn.parameters;
+
+public class Student {
+    private int studentId;
+    private String name;
+    public Student(int studentId, String name) {
+        this.studentId = studentId;
+        this.name = name;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public String getName() {
+        return name;
+    }
+}
